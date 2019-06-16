d4c418e9844e3e5cb3d689f2141f17ce9e0e400c
nathan0 <51887600+nathan0@users.noreply.github.com>
2019-06-16 15:24:02 -0300

    Add files via upload

---
+++ n/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1560709323,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowleft-sheet0.png",
+		"images/arrowright-sheet0.png",
+		"images/arrowdown-sheet0.png",
+		"images/arrowup-sheet0.png",
+		"images/backgroundanimate-sheet0.png",
+		"images/enemycontroller-sheet0.png",
+		"images/bottomdestroyer-sheet0.png",
+		"images/energyframeui-sheet0.png",
+		"images/energyui-sheet0.png",
+		"images/powerupcontrol-sheet0.png",
+		"images/barreiradir-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
